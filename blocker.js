2d5eab83f15c8f911c6b450694c18b376c2057ba
Jason Zhang <zjjc123@hotmail.com>
2022-08-29 12:28:05 -0700

    Giant Doomscroll Text

--- blocker.js
+++ blocker.js
@@ -1,5 +1,6 @@
 const SCROLL_LIMIT = 3000;
 let scrollDistance = 0;
+let warningOn = false;
 
 addEventListener("scroll", (e) => {
     const scrollDelta = document.documentElement.scrollTop - scrollDistance;
@@ -7,7 +8,18 @@ addEventListener("scroll", (e) => {
     console.log(scrollDelta)
     if (scrollDelta > 0) {
         if (scrollDistance > SCROLL_LIMIT) {
-            alert("DOOMSCROLL DETECTED");
+            displayWarning()
         }
     }
 });
+
+
+const displayWarning = () => {
+    if (!warningOn) {
+        const warning = document.createElement('div');
+        warning.style = "height: 100%; position: fixed; width: 100%; z-index: 9000; display: flex; justify-content: center; flex-direction: column;"
+        warning.innerHTML = `<h1 style="color: red; font-family: sans-serif; font-weight: bolder; text-align: center; font-size: 10vw; z-index: 9000;">DOOMSCROLL DETECTED</h1>`;
+        document.body.insertAdjacentElement('afterbegin', warning);
+        warningOn = true;
+    }
+}
